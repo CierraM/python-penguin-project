1ed3080374c515fa5afc10525f302b350fec62db
Cierra <4cibear@gmail.com>
2021-03-08 16:02:36 -0700

    added assets folder and setup for sound class

---
+++ penguin/game/sound.py
@@ -0,0 +1,5 @@
+# This file is in charge of playing sounds. We need to give it access to the sound files. Pass in the sound to be played, and play it.
+
+{squawk: "",
+ main_background: "",
+ walking: ""}
