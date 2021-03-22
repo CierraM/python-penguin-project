cd2183c8549d4840ca078a55b07dcd1fa6e2fcfa
Cierra <4cibear@gmail.com>
2021-03-22 16:43:32 -0600

    added room 3

--- penguin/game/room.py
+++ penguin/game/room.py
@@ -5,6 +5,8 @@ class Room:
     def __init__(self):
         # You may want many lists. Lists for coins, monsters, etc.
         self.wall_list = None
+        self.width = 0
+        self.height = 0
 
         # This holds the background images. If you don't want changing
         # background images, you can delete this part.
