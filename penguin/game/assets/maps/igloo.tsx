f7f51823b00b594637906b331423195b4d785676
Cierra <4cibear@gmail.com>
2021-03-25 13:40:06 -0600

    edited igloo tsx

--- penguin/game/assets/maps/igloo.tsx
+++ penguin/game/assets/maps/igloo.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="igloo" tilewidth="32" tileheight="32" tilecount="12" columns="6">
- <image source="igloo.png" width="192" height="64"/>
+<tileset version="1.4" tiledversion="1.4.3" name="igloo" tilewidth="32" tileheight="32" tilecount="16" columns="4">
+ <image source="igloo.png" width="128" height="128"/>
 </tileset>
